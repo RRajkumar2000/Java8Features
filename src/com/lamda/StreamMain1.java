c3b191390ec00d70bfe27b1738dd24f568b3a38a
RRajkumar2000 <96760519+RRajkumar2000@users.noreply.github.com>
2022-02-04 11:13:16 +0530

    short change

--- src/com/lamda/StreamMain1.java
+++ src/com/lamda/StreamMain1.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 public class StreamMain1 {
     public static void main(String[] args) {
         List<Integer> list1 = List.of(2, 4, 50, 21);
-        List<Integer> newlist = list1.stream().filter(i -> i > 30).collect(Collectors.toList());
+        List<Integer> newlist = list1.stream().filter(i -> i < 30).collect(Collectors.toList());
         System.out.println(newlist);
     }
 }
